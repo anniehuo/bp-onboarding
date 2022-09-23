6deb474c0555700b2e0d1aaa16cf726d08f7dda0
annie <annie@annies-MacBook-Pro-2.local>
2022-09-22 23:44:26 -0700

    barebones

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainbox">
+
+        <div className="textbox">
+          <div className='welcome'>
+            Welcome Back!
+          </div>
+          <div className='signin'>
+            Sign in to continue
+          </div>
+        </div>
+
+        <div className='labelbox'>
+          <div className= 'text1'>
+            <p>
+              email
+            </p>
+            <p>
+              password
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
+
+    
   );
 }
+
+
 
 export default App;
